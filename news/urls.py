fbe2b534ba9161ef2d5c0eaa8167e588b6b0eb29
jaumeguasch <jguaschllobera@gmail.com>
2022-03-15 12:04:57 +0100

    added login functionality

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('lector_register/', views.LectorRegister.as_view(), name='lector_register'),
     path('periodista_register/', views.PeriodistaRegister.as_view(), name='periodista_register'),
+    path('login/', views.login_request, name='login'),
+    path('home/', name='login')
+
 ]
