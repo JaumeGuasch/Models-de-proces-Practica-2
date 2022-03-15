8f42fd47214e08f60dd4980b4e5f3c21cbd8cdf0
jaumeguasch <jguaschllobera@gmail.com>
2022-03-15 12:08:33 +0100

    added login functionality

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .import views
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('lector_register/', views.LectorRegister.as_view(), name='lector_register'),
     path('periodista_register/', views.PeriodistaRegister.as_view(), name='periodista_register'),
     path('login/', views.login_request, name='login'),
-    path('home/', name='login')
+    path('home/', TemplateView.as_view(template_name='home.html'),name='home'),
 
 ]
