e0290e10f1f812e8fe4446a7539b97d61f943776
llobera00 <o.llobera00@gmail.com>
2022-03-15 11:45:57 +0100

    Classe Valoració i modificació classe Noticia a models.py

--- news/models.py
+++ news/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+# Falta: classe valoració, periodista com a foreign key de Noticia
 
 class User(AbstractUser):
     is_periodista = models.BooleanField(default=False)
@@ -25,3 +28,16 @@ class Noticia(models.Model):
     titol = models.CharField(max_length=20, null=False, default='DEFAULT VALUE')
     subtitol = models.CharField(max_length=120, null=False, default='DEFAULT VALUE')
     cos = models.CharField(max_length=99999, null=False, default='DEFAULT VALUE')
+    periodista = models.ForeignKey(Periodista, default='', on_delete=models.CASCADE)
+
+
+class Valoracio(models.Model):
+    puntuacio = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
+    lector = models.ForeignKey(Noticia, default=1, on_delete=models.CASCADE)
+    noticia = models.ForeignKey(Lector, default='', on_delete=models.CASCADE)
